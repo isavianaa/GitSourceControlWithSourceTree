16c48d5419aaeaf86cd40223115023706a8acb05
John Wang <ddevnz@gmail.com>
2017-10-17 10:06:50 +1300

    INITIATE PROJECT

---
+++ GitWorkshop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitWorkshop
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
